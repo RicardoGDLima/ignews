3ceab783909fd2da2691c55a41199f3ff1794320
Ricardo <rickgomeslima@gmail.com>
2021-10-26 20:14:25 -0300

    Subscribe Button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { SubscribeButon } from '../components/SubscribeButton'
 
 import styles from './home.module.scss'
 
@@ -16,8 +17,8 @@ export default function Home() {
             Get access to all the publications <br />
             <span>for $9.90 month</span>
           </p>
-       </section>
-
+          <SubscribeButon />
+       </section>     
        <img src="/images/avatar.svg" alt="Gril coding" />
      </main>
     </>
