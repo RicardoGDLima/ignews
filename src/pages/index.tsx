dcc0cd5526fe9275e849ad4d77373618356b8707
Ricardo <rickgomeslima@gmail.com>
2021-10-27 13:07:05 -0300

    static site generation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 
 import Head from 'next/head'
 import { SubscribeButon } from '../components/SubscribeButton'
@@ -37,7 +37,7 @@ export default function Home({product}: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1JozBZKjsbY4kSlh4BuKKqpZ', {
     expand: ['product']
   }) 
@@ -53,7 +53,8 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24, // a cada 24 horas
   }
 
 }
